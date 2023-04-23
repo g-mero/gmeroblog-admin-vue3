fe740c8db99ff2fb9618467ee8775f82209b2269
Gmero <2431775986@qq.com>
2023-04-23 10:08:26 +0800

    🚩注意: 部署到netlify

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,8 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
-  console.log(env)
 
   return {
     base: env.VITE_PUBLIC_PATH,
